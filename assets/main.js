4251a20b7a82764a8ac82abb484f747afc1b2e93
onadj <oliver.nad@gmail.com>
2020-07-29 20:40:06 +0000

    animation with js working

--- assets/main.js
+++ assets/main.js
@@ -1,33 +1,59 @@
-var tl = new TimelineMax({onUpdate:updatePercentage});
-var tl2 = new TimelineMax();
-const controller = new ScrollMagic.Controller();
+$(window).scroll(function () {
+    $('.animation1').each(function () {
+        var imagePos = $(this).offset().top;
+        var imageHeight = $(this).height();
+        var topOfWindow = $(window).scrollTop();
 
-tl.from('blockquote', .5, {x:200, opacity: 0});
-tl.from('span', 1, { width: 0}, "=-.5");
-tl.from('#office', 1, {x:-200, opacity: 0,ease: Power4.easeInOut}, "=-1");
-tl.from('#building', 1, {x:200, opacity: 0, ease: Power4.easeInOut}, "=-.7");
+        if (imagePos < topOfWindow + imageHeight && imagePos + imageHeight > topOfWindow) {
+            $(this).addClass("droneimage");
+        } else {
+            $(this).removeClass("droneimage");
+            console.log(imageHeight)
+        }
+    });
+});
 
-tl2.from("#box", 1, {opacity: 0, scale: 0});
-tl2.to("#box", .5, {left: "20%", scale: 1.3, borderColor: 'white', borderWidth: 12, boxShadow: '1px 1px 0px 0px rgba(0,0,0,0.09)'})
+$(window).scroll(function () {
+    $('.animation2').each(function () {
+        var imagePos = $(this).offset().top;
+        var imageHeight = $(this).height();
+        var topOfWindow = $(window).scrollTop();
 
-const scene = new ScrollMagic.Scene({
-  triggerElement: ".sticky",
-            triggerHook: "onLeave",
-            duration: "100%"
-})
-  .setPin(".sticky")
-  .setTween(tl)
-		.addTo(controller);
+        if (imagePos < topOfWindow + imageHeight && imagePos + imageHeight > topOfWindow) {
+            $(this).addClass("specification");
+        } else {
+            $(this).removeClass("specification");
+            console.log(imageHeight)
+        }
+    });
+});
 
-const scene2 = new ScrollMagic.Scene({
-  triggerElement: "blockquote"
-})
-  .setTween(tl2)
-		.addTo(controller);
+$(window).scroll(function () {
+    $('.animation3').each(function () {
+        var imagePos = $(this).offset().top;
+        var imageHeight = $(this).height();
+        var topOfWindow = $(window).scrollTop();
 
+        if (imagePos < topOfWindow + imageHeight && imagePos + imageHeight > topOfWindow) {
+            $(this).addClass("review");
+        } else {
+            $(this).removeClass("review");
+            console.log(imageHeight)
+        }
+    });
+});
 
-function updatePercentage() {
-  //percent.innerHTML = (tl.progress() *100 ).toFixed();
-  tl.progress();
-  console.log(tl.progress());
-}
+$(window).scroll(function () {
+    $('.animation4').each(function () {
+        var imagePos = $(this).offset().top;
+        var imageHeight = $(this).height();
+        var topOfWindow = $(window).scrollTop();
+
+        if (imagePos < topOfWindow + imageHeight && imagePos + imageHeight > topOfWindow) {
+            $(this).addClass("wheretobuy");
+        } else {
+            $(this).removeClass("wheretobuy");
+            console.log(imageHeight)
+        }
+    });
+});
