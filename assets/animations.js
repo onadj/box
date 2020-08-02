4f851e937fc23200dbb4d530713c9179f6bd9f50
onadj <oliver.nad@gmail.com>
2020-08-02 08:49:06 +0000

    animations

---
+++ assets/animations.js
@@ -0,0 +1,14 @@
+$(window).scroll(function () {
+    $('.animation2').each(function () {
+        var imagePos = $(this).offset().top;
+        var imageHeight = $(this).height();
+        var topOfWindow = $(window).scrollTop();
+
+        if (imagePos < topOfWindow + imageHeight && imagePos + imageHeight > topOfWindow) {
+            $(this).addClass(".move-left-to-right");
+        } else {
+            $(this).removeClass(".move-left-to-right");
+            console.log(imageHeight)
+        }
+    });
+});
